1cc6a7c144f7a6090fef039f7d7ac98d6a6f87dc
Andrew Azores <me@andrewazor.es>
2026-03-13 12:58:11 -0400

    build script fixup

--- build-local.bash
+++ build-local.bash
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -xe
+
 DIR="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "${DIR}/bin"
 
+pushd "${DIR}/cryostat-agent" || exit
+./mvnw -DskipTests clean package
+popd || exit
+cp "${DIR}"/cryostat-agent/target/cryostat-agent-*-shaded.jar "${DIR}/bin/"
+
 pushd "${DIR}/cryostat" || exit
+git submodule init && git submodule update
 ./mvnw \
     -Dquarkus.container-image.build=false \
     -Dquarkus.package.jar.type=uber-jar \
     -DskipTests \
     clean package
 popd || exit
-cp "${DIR}/cryostat/target/cryostat-*-runner.jar" "${DIR}/bin/"
-
-pushd "${DIR}/cryostat-agent" || exit
-./mvnw clean package
-popd || exit
-cp "${DIR}/cryostat-agent/target/cryostat-agent-*-shaded.jar" "${DIR}/bin/"
+cp "${DIR}"/cryostat/target/cryostat-*-runner.jar "${DIR}/bin/"
